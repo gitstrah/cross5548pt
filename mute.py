d6f160d24f80e89240e5111ac58c8918c8b01195
Andrey <gitstrah@gmail.com>
2015-12-04 22:23:03 -0600

    mute & unmute

---
+++ mute.py
@@ -0,0 +1,18 @@
+
+import printer
+import cross
+import pwmm
+import hardware
+from time import sleep
+#################################################################################
+i2c_addr = 0x1A
+log_level = printer.LogLevel.All
+#################################################################################
+
+logger = printer.Log(log_level)
+i2c = hardware.I2C(i2c_addr, logger)
+pwm = pwmm.TAS5548(i2c, logger)
+
+# mute
+pwm.mute()
+#pwm.unmute()
